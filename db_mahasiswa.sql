7b588322d45a5d0ded67d919893dfe52ac803f2c
I Made Dharma Putra <57142530+dhrmputra@users.noreply.github.com>
2023-05-17 07:51:09 -1000

    Add files via upload

--- db_mahasiswa.sql
+++ db_mahasiswa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2023 at 06:19 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Waktu pembuatan: 18 Bulan Mei 2023 pada 13.51
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbmhs`
+-- Struktur dari tabel `tbmhs`
 --
 
 CREATE TABLE `tbmhs` (
@@ -33,14 +33,24 @@ CREATE TABLE `tbmhs` (
   `nim` varchar(50) DEFAULT NULL,
   `prodi` varchar(255) DEFAULT NULL,
   `tgl_lahir` date DEFAULT NULL,
-  `jk` varchar(1) DEFAULT NULL,
+  `jk` varchar(255) DEFAULT NULL,
   `id_mhs` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbmhs`
+--
+
+INSERT INTO `tbmhs` (`id`, `nama`, `nim`, `prodi`, `tgl_lahir`, `jk`, `id_mhs`) VALUES
+(19, 'Dharma Putra', '123123123', 'MTI', '2003-12-12', 'Laki-laki', 'b4a05210f21a1274d3dc78c535f8d06d'),
+(20, 'Gek Ria', '456456456', 'SK', '2002-04-05', 'Perempuan', '53d7f5dc23a7f1962a4db61d9d1ccc65'),
+(21, 'Didok Anjay', '678678678', 'DKV', '2004-03-12', 'Laki-laki', '36c6848e019eedb6e90af8dca0febf72'),
+(22, 'Putu Ayu', '890890890', 'KAB', '2004-03-02', 'Perempuan', '2a9bacc0f7462328ee9a9958d40c762c');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbuser`
+-- Struktur dari tabel `tbuser`
 --
 
 CREATE TABLE `tbuser` (
@@ -53,43 +63,44 @@ CREATE TABLE `tbuser` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tbuser`
+-- Dumping data untuk tabel `tbuser`
 --
 
 INSERT INTO `tbuser` (`id`, `nama`, `email`, `username`, `passkey`, `iduser`) VALUES
-(1, 'soho', 'soho69@gmail.com', 'sohooo', '123', '1');
+(10, 'dharmaaa', 'dharma@gmail.com', 'dharmo', '123', '15e3f94d0fe47351db1efa115ef91e42'),
+(16, 'didok anjay', 'didok@gmail.co', 'didok', '123', 'e8fb40ff50524caccdd37f7a01bf71a9');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tbmhs`
+-- Indeks untuk tabel `tbmhs`
 --
 ALTER TABLE `tbmhs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tbuser`
+-- Indeks untuk tabel `tbuser`
 --
 ALTER TABLE `tbuser`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tbmhs`
+-- AUTO_INCREMENT untuk tabel `tbmhs`
 --
 ALTER TABLE `tbmhs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
--- AUTO_INCREMENT for table `tbuser`
+-- AUTO_INCREMENT untuk tabel `tbuser`
 --
 ALTER TABLE `tbuser`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
