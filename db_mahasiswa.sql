1f5359797cec98868796a60ba9dbc99b1ca6b6ef
I Made Dharma Putra <57142530+dhrmputra@users.noreply.github.com>
2023-05-22 17:29:00 +0800

    Add files via upload

--- db_mahasiswa.sql
+++ db_mahasiswa.sql
@@ -7,6 +7,8 @@
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
+
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -19,14 +21,15 @@ SET time_zone = "+00:00";
 
 --
 -- Database: `db_mahasiswa`
---
+-- 
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `tbmhs`
 --
-
+create Database db_mahasiswa;\
+  
 CREATE TABLE `tbmhs` (
   `id` int(11) NOT NULL,
   `nama` varchar(30) DEFAULT NULL,
