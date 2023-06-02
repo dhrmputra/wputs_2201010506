a0948972a7b510037266f8a86ee0e1fb3ba673f1
I Made Dharma Putra <57142530+dhrmputra@users.noreply.github.com>
2023-06-02 08:10:10 -1000

    mhsnew.php

--- mhs/mhsnew.php
+++ mhs/mhsnew.php
@@ -7,7 +7,7 @@
                         <div class="col-sm-6 text-left">
                             <h5 class="card-category">List Mahasiswa</h5>
                             <h2 class="card-title">Total Daftar Mahasiswa <b>
-                                    <?php $stt = mysqli_query($cnn, "SELECT * FROM tbMHS");
+                                    <?php $stt = mysqli_query($cnn, "SELECT * FROM tbmhs");
                                     echo mysqli_num_rows($stt); ?></b></h2>
                         </div>
                     </div>
